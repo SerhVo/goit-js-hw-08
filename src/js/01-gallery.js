7273cec757399c826bc027167fb0a84150797a27
Serhii Vorotnoi <svorotnoy@gmail.com>
2022-11-02 17:21:48 +0200

    8.1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -19,14 +19,13 @@ function createGalleryMarkup(galleryItems) {
   return galleryItems
     .map(
       ({ preview, original, description }) =>
-        `<li class="gallery__item">
+        `
 <a class="gallery__link" href=${original}>
     <img class="gallery__image"
     src=${preview} 
     data-source=${original}
     alt=${description}/>
-</a>
-</li>`
+</a>`
     )
     .join('');
 }
