7c66ddbfb74de18d1b71a1da59c4d72a7e446818
Serhii Vorotnoi <svorotnoy@gmail.com>
2022-11-04 18:15:02 +0200

    8.3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,3 +5,31 @@
 // Під час завантаження сторінки перевіряй стан сховища, і якщо там є збережені дані, заповнюй ними поля форми. В іншому випадку поля повинні бути порожніми.
 // Під час сабміту форми очищуй сховище і поля форми, а також виводь у консоль об'єкт з полями email, message та їхніми поточними значеннями.
 // Зроби так, щоб сховище оновлювалось не частіше, ніж раз на 500 мілісекунд. Для цього додай до проекту і використовуй бібліотеку lodash.throttle.
+
+import throttle from 'lodash.throttle';
+const formElem = document.querySelector('.feedback-form');
+let data = {};
+formElem.addEventListener('input', throttle(onInput, 500));
+
+function onInput(event) {
+  data[event.target.name] = event.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(data));
+}
+
+if (localStorage.getItem('feedback-form-state')) {
+  const { email, message } = JSON.parse(
+    localStorage.getItem('feedback-form-state')
+  );
+  formElem.email.value = email;
+  formElem.message.value = message;
+  data.email = email;
+  data.message = message;
+}
+
+formElem.addEventListener('submit', onSubmit);
+
+function onSubmit(event) {
+  event.preventDefault();
+  localStorage.removeItem('feedback-form-state');
+  formElem.reset();
+}
